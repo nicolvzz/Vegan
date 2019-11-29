61da004c742dcf37737aba73c174e5770cde34c8
Nicolas <nipobletem@gmail.com>
2019-11-28 23:09:47 -0300

    agregando login y eliminar

--- vegan/urls.py
+++ vegan/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.detalle_post, name='detalle_post'),
     path('post/nuevo', views.nuevo_post, name='nuevo_post'),
     path('post/<int:pk>/edit/', views.editar_post, name='editar_post'),
+    path('EliminarPost/<int:pk>/', views.eliminar_post, name='eliminar_post'),
+    path('logout', views.logout),
+     path('login', views.login, name='login'),
+
 ]
--- vegan/views.py
+++ vegan/views.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as do_logout
+from django.contrib.auth import login as do_login
 
 # Create your views here.
 
@@ -44,3 +50,38 @@ def editar_post(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'vegan/editar_post.html', {'form': form})
+
+def eliminar_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_lista')
+
+def logout(request):
+    do_logout(request)
+    return redirect('login')
+def login(request):
+    # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=username, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('/')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, "vegan/login.html", {'form': form})
+
+    
